3bf885c1cc4ea962bbb15ee8dfaa820b3eb9fb2f
Muhannad Saeed Almutairi <muhnndsaeed@Muhannads-MacBook-Pro.local>
2022-10-06 07:51:42 +0300

    first commit

---
+++ todolist/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+
+import 'addtask.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(HomeScreen());
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "todo app",
+      theme: ThemeData(
+        primaryColor: Colors.greenAccent[700],
+      ),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final fb = FirebaseDatabase.instance;
+  @override
+  Widget build(BuildContext context) {
+    final ref = fb.ref().child('todos');
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => addtask(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Todo list App',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.black38,
+      ),
+      body: FirebaseAnimatedList(
+        query: ref,
+        shrinkWrap: true,
+        itemBuilder: (context, snapshot, animation, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  tileColor: Colors.indigo[100],
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.red[900],
+                    ),
+                    onPressed: () {
+                      ref.child(snapshot.key!).remove();
+                    },
+                  ),
+                  title: Text(
+                    snapshot.value.toString(),
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
